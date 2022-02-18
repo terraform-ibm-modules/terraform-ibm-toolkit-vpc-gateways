5cf6ebf6349b582a4702620b68298cd25af2e587
Sean Sundberg <seansund@us.ibm.com>
2022-02-17 19:01:07 -0800

    Removes unused subnet_count variable
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-gateways.tf
+++ test/stages/stage2-gateways.tf
@@ -4,7 +4,6 @@ module "gateways" {
   resource_group_id = module.resource_group.id
   region            = var.region
   vpc_name          = module.vpc.name
-  subnet_count      = var.vpc_subnet_count
   enabled           = var.enabled
 }
 
